ef374be6fa827b0176828f3d11de6fdfc4d51d48
lukesgon <lucasdasgoncalves@gmail.com>
2023-09-21 18:36:19 -0300

    Desenvolvimento 8 concluído.

---
+++ Desenvolvimento_8.py
@@ -0,0 +1,19 @@
+'''
+# Projeto para o desenvolvimento da atividade 8, do curso Proz de Introdução à programação, pelo aluno Lucas da Silva Gonçalves.
+# Project for activity 8, of the course Proz of Introducing Programming by Lucas da Silva Gonçalves.
+
+Copie o array apresentado embaixo no seu editor de código, e imprima no terminal:
+A quantidade de elementos que ele possui, o dado salvo no índice 2, o dado salvo no índice 9, e dado salvo no índice 14.
+
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+'''
+
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+print("A lista contém " + str(len(lista_musicos)) + " músicos")
+
+print("O músico número 2 é: " + lista_musicos[2])
+
+print("O músico número 9 é: " + lista_musicos[9])
+
+print("O músico número 14 é: " + lista_musicos[14])
